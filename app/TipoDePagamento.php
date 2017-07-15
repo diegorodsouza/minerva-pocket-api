41e2ef034b736fd43c23c0a82775310921820619
diegorodsouza <diegorodsouza@hotmail.com>
2017-07-14 21:02:14 -0300

    *model tipodepagamento com tabela correta

--- app/TipoDePagamento.php
+++ app/TipoDePagamento.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class TipoDePagamento extends Model
 {
+    protected $table = 'tipo_de_pagamento';
+
     protected $fillable = [
       'descricao'
     ];
