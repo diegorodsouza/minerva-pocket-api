cb0e2382faa1c4416057493e8a2845173a84f071
diegorodsouza <diegorodsouza@hotmail.com>
2017-07-17 23:38:26 -0300

    *tentativa 3 de fazer a funcao funcionar na view

--- resources/views/auth/alimentacao/index.blade.php
+++ resources/views/auth/alimentacao/index.blade.php
@@ -32,7 +32,7 @@
           <td>{{$local->id}}</td>
           <td>{{$local->nome}}</td>
           <td>{{$local->preco}}</td>
-          <td>{{$this->getLocalizacao($local->id)}}</td>
+          <td>{{App\Alimentacao::getLocalizacao($local->id)}}</td>
           <td>{{$local->imagem}}</td>
           <td>
             <!-- <form action="{{ route('DestroyAlimentacao', $local->id) }}" method="post">
