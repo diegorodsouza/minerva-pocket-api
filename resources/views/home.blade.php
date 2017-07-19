bfd2f9d92646ce3aa116cc9fd0522282e2484505
diegorodsouza <diegorodsouza@hotmail.com>
2017-07-18 21:11:20 -0300

    +destroy de alimentacao

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,13 +8,20 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    <ul>
-                      <li><a href="{{ route('TipoDePagamento') }}"> Gerir Formas de Pagamento</a></li>
-                      <li><a href="{{ route('TipoDeComida') }}"> Gerir Tipos de Serviço de Comida</a></li>
-                      <li><a href="{{ route('TipoDeAcademico') }}"> Gerir Tipos de Serviço Acadêmico</a></li>
-                      <li><a href="{{ route('CentroPonto') }}"> Gerir Centros e Pontos de Ônibus</a></li>
-                      <li><a href="{{ route('Alimentacao') }}"> Gerir Locais de Alimentação</a></li>
-                    </ul>
+                  <h2>Centros e Pontos de Ônibus</h2>
+                  <ul>
+                    <li><a href="{{ route('CentroPonto') }}"> Gerir Centros e Pontos de Ônibus</a></li>
+                  </ul>
+                  <h2>Alimentação></h2>
+                  <ul>
+                    <li><a href="{{ route('TipoDePagamento') }}"> Gerir Formas de Pagamento</a></li>
+                    <li><a href="{{ route('TipoDeComida') }}"> Gerir Tipos de Serviço de Comida</a></li>
+                    <li><a href="{{ route('Alimentacao') }}"> Gerir Locais de Alimentação</a></li>
+                  </ul>
+                  <h2>Acadêmico</h2>
+                  <ul>
+                    <li><a href="{{ route('TipoDeAcademico') }}"> Gerir Tipos de Serviço Acadêmico</a></li>
+                  </ul>
                 </div>
             </div>
         </div>
