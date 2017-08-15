d95a544f87d8be2bd31fa84ef6eeb4a1c4c4efc1
diegorodsouza <diegorodsouza@hotmail.com>
2017-08-15 11:52:56 -0300

    +hash admin

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,9 +12,9 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
       DB::table('users')->insert([
-                 'name' => "Admin",
-                 'email' => "admin@email.com",
-                 'password' => bcrypt('admin'),
+                 'name' => "DevMob UFRJ",
+                 'email' => "devmobufrj@gmail.com",
+                 'password' => "$2a$06$NPbu.pmB0Uii6tf91SzNp.PPMDS.q7EIISzXuYc19CuE7JwcuD6Yi",
              ]);
     }
 }
