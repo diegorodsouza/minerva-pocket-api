8a2bbf8d8fe289ff54efff0eae1f8acde68ea26c
diegorodsouza <diegorodsouza@hotmail.com>
2017-08-31 19:22:35 -0300

    *coodenadas interno api com logica do edit

--- app/Http/Controllers/TransporteController.php
+++ app/Http/Controllers/TransporteController.php
@@ -39,7 +39,7 @@ class TransporteController extends Controller
         foreach ($pontos as $ponto){
           foreach ($transportes_localizacoes as $transporte_localizacao){
             if($ponto->id == $transporte_localizacao->localizacao_id){
-              $local = Localizacao::where('id', $transporte_localizacao->localizacao_id)->get();
+              $local = Localizacao::where('id', $ponto->id)->get();
               $pontoLoc = array(
                 'latitude' => $local->latitude,
                 'longitude'=> $local->longitude,
