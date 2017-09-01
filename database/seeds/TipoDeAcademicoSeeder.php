8ee530aa9baed25ca0eca9c96e1aaea574f6cc8a
diegorodsouza <diegorodsouza@hotmail.com>
2017-08-31 23:38:24 -0300

    *academico seeder

--- database/seeds/TipoDeAcademicoSeeder.php
+++ database/seeds/TipoDeAcademicoSeeder.php
@@ -15,19 +15,21 @@ class TipoDeAcademicoSeeder extends Seeder
               [
                 'id' => 1,
                 'descricao' => "Auditório",
-              ],
+              ]);
+      \DB::table('tipo_de_academico')->insert(    
               [
                 'id' => 2,
                 'descricao' => "Biblioteca",
-              ],
+              ]);
+      \DB::table('tipo_de_academico')->insert(  
               [
                 'id' => 3,
                 'descricao' => "Centro/Diretório Acadêmico",
-              ],
+              ]);
+      \DB::table('tipo_de_academico')->insert(  
               [
                 'id' => 4,
                 'descricao' => "Secretaria",
-              ]
-           );
+              ]);
     }
 }
